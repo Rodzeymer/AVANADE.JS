c931579c77435b78932273c36b416e5d6746af00
Rodrigo <rodzeymer@yahoo.com.br>
2020-12-11 15:51:25 -0300

    Introdução ao JS

---
+++ main.js
@@ -0,0 +1,22 @@
+var nome="Rodrigo";
+var idade=34;
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo!"
+
+idade2=n1+n2;
+
+alert(nome + " tem " + idade + " anos.")
+
+alert(idade +idade2);
+
+console.log(nome);
+
+console.log(n1+idade);
+
+console.log(frase.toLowerCase())
+
+alert(frase.replace("Japão", "Brasil"))
+
+
+alert(nome + " tem idade " + idade);
