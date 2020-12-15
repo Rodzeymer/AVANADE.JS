0a613f7a1efb8c7c3b29d7154c8e10f7f006c2a8
Rodrigo <rodzeymer@yahoo.com.br>
2020-12-15 17:12:38 -0300

    Final JS

--- main.js
+++ main.js
@@ -88,8 +88,69 @@ while (count<=5) {
     alert(count);
     
 }*/
-
+/*
 var d = new Date();
 alert(d.getMonth());
 
 
+function soma(n1, n2){
+    return n1+n2
+}
+
+
+function setReplace(frase, nome, novoNome){
+    return frase.replace(nome,novoNome)
+}
+
+/*alert(setReplace("Vai Japão", "Japão", "Brasil"));
+alert(soma(5,10));
+
+function validaIdade(idade){
+    var validar;
+
+    if(idade>=18){
+        validar = true
+
+    } else{
+        validar=false
+    }
+    return validar
+}
+
+*/
+/*
+var idade = prompt("Qual a sua idade?")
+
+validaIdade;
+
+console.log(validaIdade(idade));
+*/
+function clicou(){
+    //alert("Obrigado por clicar");
+    document.getElementById("Agradecimento").innerHTML = "Obrigado por clicar";
+    console.log(document.getElementById("Agradecimento"));
+}
+
+function outraPagina(){
+    //window.open("https://www.twitch.tv/kritsugu");
+    //window.location.href = "https://www.twitch.tv/kritsugu"
+
+}
+
+function trocar(elemento){
+    elemento.innerHTML = "Obrigado por passar por aqui antes";
+    
+}
+
+function voltar(elemento){
+    elemento.innerHTML = "O mouse saiu do canto!";
+    
+}
+
+function load(){
+    alert("Página carregada!")
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
